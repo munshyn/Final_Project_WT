0bed9214e09b24f61b093c508276ce1c21d7f4cf
Mfikrilamin <muhd.fikrilamin@gmail.com>
2022-07-25 22:40:38 +0800

    Home & login page

--- Project/Start/logout.php
+++ Project/Start/logout.php
@@ -4,11 +4,12 @@ session_start();
 // if the user is logged in, unset the session 
 if (isset($_SESSION['USER'])) { 
 	unset($_SESSION['USER']); 
-} 
+}
+
 session_destroy(); //destroy the session
 
 
 // go to login page 
-header('Location: index.php'); 
+header('Location: login.php'); 
 exit; 
 ?> 
---
+++ View/logout.php
@@ -0,0 +1,24 @@
+<?php
+session_start(); 
+
+// if the user is logged in, unset the session 
+if (isset($_SESSION['userId'])) { 
+	unset($_SESSION['userId']); 
+}
+if (isset($_SESSION['name'])) { 
+	unset($_SESSION['name']); 
+} 
+if (isset($_SESSION['level'])) { 
+	unset($_SESSION['level']); 
+} 
+if (isset($_SESSION['email'])) { 
+	unset($_SESSION['email']); 
+} 
+
+session_destroy(); //destroy the session
+
+
+// go to login page 
+header('Location: index.php'); 
+exit; 
+?> 
